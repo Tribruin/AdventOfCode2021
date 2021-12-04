19b0cf9abeae44f22d8d3264a42f492b0100c3d7
Rob Blount <tribruin@gmail.com>
2021-12-04 05:23:14 -0600

    Day 3 Finished

---
+++ Day3/main.py
@@ -0,0 +1,93 @@
+#!/Users/rblount/.pyenv/versions/AdOfCode/bin/python
+
+
+import sys
+import os
+import numpy as np
+from timeit import timeit
+
+# sys.path.append("/Users/rblount/Scripts/AdventOfCode/tools")
+
+from AOC import AOC
+
+testing = False
+
+
+def compute_gamma(lines):
+
+    y, x = len(lines), len(lines[0])
+    array = np.zeros((y, x), dtype=int)
+    for i, line in enumerate(lines):
+        for k, bit in enumerate(line):
+            array[i][k] = int(bit)
+
+    ones = np.count_nonzero(
+        array,
+        axis=0,
+    )
+
+    gamma = "".join(["1" if digit >= (y - digit) else "0" for digit in ones])
+
+    epsilon = "".join(["0" if digit == "1" else "1" for digit in gamma])
+
+    return gamma, epsilon
+
+
+def part1():
+    lines = data.read_lines()
+    gamma, epsilon = compute_gamma(lines)
+    print(int(gamma, 2) * int(epsilon, 2))
+
+
+def part2():
+    lines = data.read_lines()
+
+    o2rating = lines.copy()
+    co2rating = lines.copy()
+    line_len = len(lines[0])
+
+    for i in range(line_len):
+        o2rating_copy = o2rating.copy()
+
+        gamma, _ = compute_gamma(o2rating)
+        char = gamma[i]
+        for k in range(len(o2rating) - 1, -1, -1):
+            if char != o2rating_copy[k][i]:
+                o2rating_copy.pop(k)
+        o2rating = o2rating_copy
+        if len(o2rating) == 1:
+            # print(o2rating[0])
+            break
+
+    for i in range(line_len):
+        co2rating_copy = co2rating.copy()
+
+        _, epsilon = compute_gamma(co2rating)
+        char = epsilon[i]
+        for k in range(len(co2rating) - 1, -1, -1):
+            if char != co2rating_copy[k][i]:
+                co2rating_copy.pop(k)
+        co2rating = co2rating_copy
+        if len(co2rating) == 1:
+            # print(co2rating[0])
+            break
+
+    print(int(o2rating[0], 2) * int(co2rating[0], 2))
+
+
+def main():
+    # Get the path name and strip to the last 1 or 2 characters
+    codePath = os.path.dirname(sys.argv[0])
+    codeDate = int(codePath.split("/")[-1][3:])
+    codeYear = int(codePath.split("/")[-2])
+    print(f"Running Advent of Code for Year: {codeYear} - Day {codeDate}")
+
+    global data
+    data = AOC(codeDate, codeYear, test=testing)
+
+    print(f"Time for Part One: {timeit(part1, number=1)}")
+    print(f"Time for Part Two: {timeit(part2, number=1)}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ Day4/main.py
@@ -0,0 +1,38 @@
+#!/Users/rblount/.pyenv/versions/AdOfCode/bin/python
+
+
+import sys
+import os
+from timeit import timeit
+
+# sys.path.append("/Users/rblount/Scripts/AdventOfCode/tools")
+
+from AOC import AOC
+
+testing = True
+
+
+def part1():
+    pass
+
+
+def part2():
+    pass
+
+
+def main():
+    # Get the path name and strip to the last 1 or 2 characters
+    codePath = os.path.dirname(sys.argv[0])
+    codeDate = int(codePath.split("/")[-1][3:])
+    codeYear = int(codePath.split("/")[-2])
+    print(f"Running Advent of Code for Year: {codeYear} - Day {codeDate}")
+
+    global data
+    data = AOC(codeDate, codeYear, test=testing)
+
+    print(f"Time for Part One: {timeit(part1, number=1)}")
+    print(f"Time for Part Two: {timeit(part2, number=1)}")
+
+
+if __name__ == "__main__":
+    main()
