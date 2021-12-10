78f7cbf35fad2db3df220faf5ad0d0fd866cc980
Rob Blount <tribruin@gmail.com>
2021-12-10 08:07:34 -0600

    Day 10 Complete

---
+++ Day10/main.py
@@ -0,0 +1,90 @@
+#!/Users/rblount/.pyenv/versions/AdOfCode/bin/python
+
+
+import sys
+import os
+from AOC import AOC
+
+testing = False
+
+open_markers = ["(", "[", "{", "<"]
+close_markers = [")", "]", "}", ">"]
+marker_values = [3, 57, 1197, 25137]
+corrupt_values = [1, 2, 3, 4]
+
+
+def part1(data):
+
+    total_sum = 0
+    lines_to_remove = list()
+
+    for x, line in enumerate(data):
+        current_open_markers = list()
+        for i in line:
+            if i in open_markers:
+                current_open_markers.append(i)
+            else:
+                last_char = current_open_markers.pop()
+                if not open_markers.index(last_char) == close_markers.index(i):
+                    total_sum += marker_values[close_markers.index(i)]
+                    # print(x, i)
+                    lines_to_remove.append(x)
+                    break
+
+    print(total_sum)
+    return lines_to_remove
+
+
+def part2(data, corrupt_lines):
+
+    for i in corrupt_lines[::-1]:
+        data.pop(i)
+
+    missing_markers_values = list()
+
+    for x, line in enumerate(data):
+        missing_markers = list()
+        current_open_markers = list()
+        for i in line:
+            if i in open_markers:
+                current_open_markers.append(i)
+            else:
+                last_char = current_open_markers[-1]
+                if open_markers.index(last_char) == close_markers.index(i):
+                    current_open_markers.pop()
+
+        for i in current_open_markers[::-1]:
+            missing_markers.append(close_markers[open_markers.index(i)])
+
+        total_value = 0
+        for i in missing_markers:
+            total_value *= 5
+            total_value += close_markers.index(i) + 1
+        missing_markers_values.append(total_value)
+        # print(x, total_value)
+
+    missing_markers_values = sorted(missing_markers_values)
+    mid_point = len(missing_markers_values) // 2
+    print(missing_markers_values[mid_point])
+
+
+def main():
+    # Get the path name and strip to the last 1 or 2 characters
+    codePath = os.path.dirname(sys.argv[0])
+    codeDate = int(codePath.split("/")[-1][3:])
+    codeYear = int(codePath.split("/")[-2])
+    print(f"Running Advent of Code for Year: {codeYear} - Day {codeDate}")
+
+    global data
+    data = AOC(codeDate, codeYear, test=testing)
+    input = data.read_lines()
+    lines = list()
+    for line in input:
+        lines.append([x for x in line])
+
+    corrupt_lines = part1(lines)
+    part2(lines, corrupt_lines)
+
+
+if __name__ == "__main__":
+    main()
--- Day9/main.py
+++ Day9/main.py
@@ -4,16 +4,63 @@
 import sys
 import os
 from AOC import AOC
+import numpy as np
+from scipy.ndimage import label
 
-testing = True
-
-
-def part1(input):
-    pass
+testing = False
 
 
-def part2(input):
-    pass
+def parse_input(data: AOC):
+
+    lines = data.read_lines()
+    num_array = np.genfromtxt(lines, dtype=int, delimiter=1)
+    num_array = np.pad(num_array, 1, mode="constant", constant_values=9)
+
+    return num_array
+
+
+def get_neighbors(array: np.array, y: int, x: int) -> list():
+
+    adjecent = [
+        array[y - 1][x],
+        array[y + 1][x],
+        array[y][x - 1],
+        array[y][x + 1],
+    ]
+
+    return adjecent
+
+
+def part1(floor_array: np.array):
+    y_size, x_size = np.shape(floor_array)
+    low_points = np.full((y_size, x_size), False, dtype=bool)
+
+    for y in range(1, y_size - 1):
+        for x in range(1, x_size - 1):
+            adjecent = get_neighbors(floor_array, y, x)
+
+            # check if lowest
+            # Mark the map True or False
+            low_points[y][x] = (
+                floor_array[y][x] < adjecent[0]
+                and floor_array[y][x] < adjecent[1]
+                and floor_array[y][x] < adjecent[2]
+                and floor_array[y][x] < adjecent[3]
+            )
+
+    low_point_heights = floor_array * low_points
+    print(np.sum(low_points) + np.sum(low_point_heights))
+
+
+def part2(floor_array: np.array):
+
+    # Used code from https://gitlab.com/AsbjornOlling/aoc2021/-/blob/master/09/solve.py
+    # Did not know about label or bincount
+
+    basins, _ = label(floor_array != 9)
+    basin_areas = np.bincount(basins[basins != 0])
+    top_three = np.sort(basin_areas)[-3:]
+    print(top_three[0] * top_three[1] * top_three[2])
 
 
 def main():
@@ -25,10 +72,10 @@ def main():
 
     global data
     data = AOC(codeDate, codeYear, test=testing)
-    input = data.read_lines()
+    floor_array = parse_input(data)
 
-    part1(input)
-    part2(input)
+    part1(floor_array)
+    part2(floor_array)
 
 
 if __name__ == "__main__":
